397e07bf84b88dc417fc7736020b38e2889d8be3
Miner205 <arthurdonnat05@gmail.com>
2026-05-07 18:27:24 +0200

    completed the DatabaseConfig class
    to provide a valid JDBC connection to our ArtConnect database

--- ArtConnectPro-App/src/main/java/com/project/artconnect/config/DatabaseConfig.java
+++ ArtConnectPro-App/src/main/java/com/project/artconnect/config/DatabaseConfig.java
@@ -1,11 +1,30 @@
 package com.project.artconnect.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Database configuration constants.
  * TODO: Students should update these with their own MySQL credentials.
  */
 public class DatabaseConfig {
-    public static final String URL = "jdbc:mysql://localhost:3306/artconnect_db";
+    public static final String URL = "jdbc:mysql://localhost:3306/artconnect";
     public static final String USER = "root";
-    public static final String PASSWORD = "password"; // CHANGE ME
+    public static final String PASSWORD = "password"; //to change
+
+    /** Private constructor: utility class, no instantiation. */
+    private DatabaseConfig() {}
+
+    /**
+     * Returns a new JDBC connection to the ArtConnect database.
+     *
+     * @return an open {@link Connection}
+     * @throws SQLException if the connection fails
+     *                      DONE: completed this method.
+     */
+    public static Connection getConnection() throws SQLException {
+        // DONE: return a JDBC connection using DriverManager
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
