07685e8396bc14e31a664bb4fe7bffca2534ea82
Miner205 <arthurdonnat05@gmail.com>
2026-05-11 23:56:35 +0200

    databaseconfig.java modif

--- ArtConnectPro-App/src/main/java/com/project/artconnect/config/DatabaseConfig.java
+++ ArtConnectPro-App/src/main/java/com/project/artconnect/config/DatabaseConfig.java
@@ -11,20 +11,8 @@ import java.sql.SQLException;
 public class DatabaseConfig {
     public static final String URL = "jdbc:mysql://localhost:3306/artconnect";
     public static final String USER = "root";
-    public static final String PASSWORD = "aa"; // my mysql db password
+    public static final String PASSWORD = "mymdp"; // my mysql db password
 
     /** Private constructor: utility class, no instantiation. */
     private DatabaseConfig() {}
-
-    /**
-     * Returns a new JDBC connection to the ArtConnect database.
-     *
-     * @return an open {@link Connection}
-     * @throws SQLException if the connection fails
-     *                      DONE: completed this method.
-     */
-    public static Connection getConnection() throws SQLException {
-        // DONE: return a JDBC connection using DriverManager
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
 }
